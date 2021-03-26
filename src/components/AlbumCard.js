3893bb4ad096b231a7c84e51e688d16f10e228dc
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-03-26 19:34:48 +0100

    create album card component with delete button

---
+++ src/components/AlbumCard.js
@@ -0,0 +1,39 @@
+import React from "react";
+import Card from "@material-ui/core/Card";
+import CardHeader from "@material-ui/core/CardHeader";
+import CardContent from "@material-ui/core/CardContent";
+import { IconButton, Typography } from "@material-ui/core";
+import { DeleteOutlined } from "@material-ui/icons";
+import { makeStyles } from "@material-ui/core/styles";
+
+const useStyles = makeStyles({
+  root: {
+    width: "100%",
+    height: "100%",
+  },
+});
+
+export default function AlbumCard({ album, handleDelete }) {
+  const classes = useStyles();
+
+  return (
+    <div>
+      <Card elevation={4} className={classes.root}>
+        <CardHeader
+          action={
+            <IconButton onClick={() => handleDelete(album.id)}>
+              <DeleteOutlined />
+            </IconButton>
+          }
+          title={album.album + " by " + album.artist}
+          subheader={album.format}
+        />
+        <CardContent>
+          <Typography variant="body2" color="textSecondary">
+            {album.details}{" "}
+          </Typography>
+        </CardContent>
+      </Card>
+    </div>
+  );
+}
