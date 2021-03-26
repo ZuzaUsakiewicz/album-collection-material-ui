b259a4e121010056f713f188f83ef961488fb97e
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-03-26 19:35:46 +0100

    implement delete album card from database and page function

--- src/pages/Albums.js
+++ src/pages/Albums.js
@@ -1,21 +1,35 @@
 import React, { useState, useEffect } from "react";
+import Grid from "@material-ui/core/Grid";
+import Paper from "@material-ui/core/Paper";
+import { Container } from "@material-ui/core";
+import AlbumCard from "../components/AlbumCard";
 
 export default function Albums() {
   const [albums, setAlbums] = useState([]);
+
   useEffect(() => {
     fetch("http://localhost:8000/albums")
       .then((res) => res.json())
       .then((data) => setAlbums(data));
   }, []);
+
+  const handleDelete = async (id) => {
+    await fetch("http://localhost:8000/albums/" + id, { method: "DELETE" });
+    const newAlbums = albums.filter((album) => album.id !== id);
+    setAlbums(newAlbums);
+  };
+
   return (
-    <div>
-      {albums.map((album) => (
-        <div key={album.id}>
-          <h4>{album.artist}</h4>
-          <p>{album.album}</p> <span>{album.format}</span>
-          <p>{album.details}</p>
-        </div>
-      ))}
-    </div>
+    <Container>
+      <Grid container spacing={3}>
+        {albums.map((album) => (
+          <Grid item key={album.id} xs={12} md={6} lg={4}>
+            <Paper>
+              <AlbumCard album={album} handleDelete={handleDelete} />
+            </Paper>
+          </Grid>
+        ))}
+      </Grid>
+    </Container>
   );
 }
