0b2722fdfdc1d7756b586651fdbe6b4b9ada342b
Zuzanna Usakiewicz <z.usakiewicz@gmail.com>
2021-03-31 11:30:31 +0200

    add dialog modal with delete confirm

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,27 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 import Card from "@material-ui/core/Card";
 import CardHeader from "@material-ui/core/CardHeader";
 import CardContent from "@material-ui/core/CardContent";
 import { IconButton, Typography } from "@material-ui/core";
 import { DeleteOutlined } from "@material-ui/icons";
 import { makeStyles } from "@material-ui/core/styles";
+import Button from "@material-ui/core/Button";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
 
 const useStyles = makeStyles({
   root: {
     width: "100%",
     height: "100%",
+    textTransform: "capitalize",
   },
 });
 
 export default function AlbumCard({ album, handleDelete }) {
   const classes = useStyles();
+  const [open, setOpen] = useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
       <Card elevation={4} className={classes.root}>
         <CardHeader
           action={
-            <IconButton onClick={() => handleDelete(album.id)}>
+            // <IconButton onClick={() => handleDelete(album.id)}>
+            //   <DeleteOutlined />
+            // </IconButton>
+            <IconButton onClick={handleClickOpen}>
               <DeleteOutlined />
             </IconButton>
           }
@@ -37,6 +56,32 @@ export default function AlbumCard({ album, handleDelete }) {
           </Typography>
         </CardContent>
       </Card>
+      <Dialog
+        open={open}
+        onClose={handleClose}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">{"Delete the album?"}</DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Are you sure you want to delete the album? It will be gone
+            permanently.
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <Button onClick={handleClose} color="primary">
+            Cancel
+          </Button>
+          <Button
+            onClick={() => handleDelete(album.id)}
+            color="secondary"
+            autoFocus
+          >
+            Delete
+          </Button>
+        </DialogActions>
+      </Dialog>
     </div>
   );
 }
